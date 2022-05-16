938d8241d5c6318fd543ffcc8e5915bc5ddc6eaf
Doug A <douganderson444@gmail.com>
2022-05-16 08:56:18 -0300

    gh pages config

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,58 @@
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		prerender: { default: true },
+		paths: {
+			// change below to your repo name
+			base: process.env.NODE_ENV === 'development' ? '' : '/ipfs-vite-svelte-kit'
+		},
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		},
+		vite: {
+			resolve: {
+				alias: {
+					'@douganderson444/ipfs-vite-svelte-kit': path.resolve('src/lib')
+				}
+			},
+			server: {
+				fs: {
+					strict: false
+				}
+			},
+			build: {
+				rollupOptions: {
+					plugins: [],
+					output: {
+						minifyInternalExports: false,
+						compact: false,
+						sourcemap: true
+					}
+				},
+				minify: false,
+				sourcemap: true,
+				optimization: {
+					minimize: false
+				}
+			},
+			optimization: {
+				minimize: false
+			}
+		}
 	}
 };
 
